4e4116d4b30938d2f034ecaafe08398a1ef6fcda
Anushka <anushkameena0612@gmail.com>
2025-07-27 20:22:13 +0530

    The header now shows the number of tasks that are not completed yet and also styling changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 export function Header(props) {
     const { todos } = props
-    const todoLength = todos.length
+    const activeTodos = todos.filter(todo => !todo.complete)
+    const todoLength = activeTodos.length
     const isPlural = todoLength !== 1
     const taskText = isPlural ? "tasks" : "task"
+    
 
     
     return (
         <header>
-            <h1>TO-DO LIST !</h1>
+            <div className="todo-heading">TO-DO LIST !</div>
             <h1 className="text-gradient">You have {todoLength} {taskText} left.</h1>
         </header>
     )
