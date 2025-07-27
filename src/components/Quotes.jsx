54edf45fcd9d426c7d0c4d81bb70ef1a0e874ed2
Anushka <anushkameena0612@gmail.com>
2025-07-27 22:13:55 +0530

    Resolving bug

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,8 +1,8 @@
 export function Quotes() {
     return(
         <div className="quotes">
-            <img src="./public/images/smallsteps.jpg" alt="You are exactly where you meant to be" />
-            <img src="./public/images/catimage.jpg" alt="Believe in yourself" />
+            <img src="/images/smallsteps.jpg" alt="You are exactly where you meant to be" />
+            <img src="/images/catimage.jpg" alt="Believe in yourself" />
             
         </div>
     )
